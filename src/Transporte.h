bab4c4cc7257982ef91b3f4111d739f8c8390c3f
Janilson Varela <janilsonjvs@gmail.com>
2026-07-01 10:32:49 -0300

    feat: header transporte

---
+++ src/Transporte.h
@@ -0,0 +1,44 @@
+#ifndef TRANSPORTE_H
+#define TRANSPORTE_H
+
+#include <string>
+#include "Cidade.h"
+
+class Transporte {
+    private:
+        std::string nome;
+        char tipo;
+        int capacidade;
+        int velocidade;
+        int distancia_entre_descansos;
+        int tempo_de_desconso;
+        int tempo_de_descanso_atual;
+        Cidade* localAtual;
+        bool emMovimento;
+
+    public:
+        Transporte(const std::string nome, char tipo, int capacidade, 
+            int velocidade, int distancia_entre_descansos, int tempo_de_desconso,
+            Cidade* localAtual);
+        
+        std::string getNome() const;
+        char getTipo() const;
+        int getCapacidade() const;
+        int getVelocidade() const;
+        int getDistanciaEntreDescansos() const;
+        int getTempoDescanso() const;
+        int getTempoDescansoAtual() const;
+        Cidade* getLocalAtual() const;
+        bool getEmMovimento() const;
+
+        void setLocalAtual(Cidade* local);
+        void setEmMovimento(bool estado);
+        void adicionarTempoDescanso(int horas);
+        void resetarTempoDescanso();
+
+        double calcularTempoViagem(int distancia) const;
+
+};
+
+
+#endif
